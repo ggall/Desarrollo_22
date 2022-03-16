fbc33918585b5b0e629b267c8be9f764add612af
Sergio Gallegos <isc.sergio.g@gmail.com>
2022-03-15 19:53:35 -0600

    ejercicios basicos
    Llamado desde archivo externo.
    Llamado desde el mismo html.
    Muestra imagenes

--- vuejs_base/basic.js
+++ vuejs_base/basic.js
@@ -4,3 +4,14 @@ var app = new Vue({
         message: 'Hello Luis!'
     }
 });
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        bucl:[
+            { message:"primer pagina ok"  },
+            { message:"Segunda pagina Nok" },
+            { message:'Tercer pagina ok' },
+        ]
+    }
+  })
